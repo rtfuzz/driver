b9ec8a8860c37ba03cb234fbbf9096a812e5b323
rtfuzz <rtfuzz@gmail.com>
2016-07-03 13:09:31 -0300

    driver app
    teste

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={		
+	'ApiUrl': "http://apetitego.com.br/driver/api",
+	'DialogDefaultTitle': "ApetiteGO, seu apetite bem cuidado",	
+	'PushProjectID': "172997422108",	
+	'APIHasKey': "FuzzSFJDdfsaf3i214u98@!#$theStarHash"
+};
